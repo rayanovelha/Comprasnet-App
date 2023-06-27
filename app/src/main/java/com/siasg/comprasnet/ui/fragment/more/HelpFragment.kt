0bbf6ff20896c637ab4ce3043bb8c3cf22af1339
Rayan Mota Peixoto Cordeiro <rayanmpcordeiro@gmail.com>
2023-06-27 15:19:50 -0300

    otimizações em transições das telas e adição de novas perguntas ao FAQ do app

--- app/src/main/java/com/siasg/comprasnet/ui/fragment/more/HelpFragment.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/fragment/more/HelpFragment.kt
@@ -21,17 +21,45 @@ class HelpFragment : Fragment() {
         binding.fragment = this
 
         val text = "<br>" +
-                "<big><i><b><font color='black'>Qual é o objetivo desse aplicativo?</font></b></i></big><br>" +
-                "<font color='#646464';align='justify'>&nbsp; O objetivo do aplicativo móvel desenvolvido é " +
-                "permitir que funcionários públicos e cidadãos comuns fiscalizem e consultem os contratos públicos realizados pelo governo federal. Ele fornece acesso " +
-                "simplificado e intuitivo a informações sobre valores, gestores, vencimentos e categorização dos contratos.</font><br><br>" +
-                "<big><i><b><font color='black'>Quais dados são disponibilizados pelo Sistema Integrado de Administração e Serviços Gerais (SIASG)?</font></b></i></big><br>" +
-                "<font color='#646464';align='justify'>&nbsp; O SIASG disponibiliza diversos dados relacionados às compras governamentais. Através da API de Compras Governamentais, " +
-                "é possível acessar informações sobre fornecedores, materiais, serviços, licitações, pregões, contratos, compras sem licitação e o plano anual de contratações.</font><br><br>" +
-                "<big><i><b><font color='black'>Como é a experiência do usuário no aplicativo?</font></b></i></big><br>" +
-                "<font color='#646464';align='justify'>&nbsp; O aplicativo foi projetado para fornecer uma experiência de usuário amigável. Possui uma interface intuitiva e fluida, utilizando o " +
-                "componente de navegação da biblioteca Android Jetpack para facilitar o acesso às funcionalidades. O aplicativo é dividido em seções, como Início, Minha Conta, Favoritos e Mais, e " +
-                "apresenta os resultados dos contratos de forma clara e sintetizada através de cartões informativos.</font><br><br>"
+                "<big><i><b><font color='black'>1. O que é o aplicativo de consultas públicas de compras governamentais?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; O aplicativo é uma plataforma desenvolvida em Kotlin, de código aberto, que permite aos usuários acessar informações sobre contratos públicos do governo federal. Ele disponibiliza dados como quantidade de contratos, vencimentos, valores, categorização e fornecedores.</font><br><br>" +
+
+                "<big><i><b><font color='black'>2. Quem pode utilizar o aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; O aplicativo pode ser utilizado por funcionários públicos e cidadãos comuns que desejam fiscalizar e consultar os contratos públicos realizados pelo governo federal.</font><br><br>" +
+
+                "<big><i><b><font color='black'>3. Qual é o objetivo do aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; O objetivo principal do aplicativo é aumentar a transparência dos órgãos e entidades públicas federais, fornecendo acesso simplificado e intuitivo às informações sobre os contratos públicos. Ele visa contribuir para o aumento do nível de transparência pública.</font><br><br>" +
+
+                "<big><i><b><font color='black'>4. Quais são os dados disponibilizados pelo aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; O aplicativo disponibiliza informações como valores dos contratos, gestores responsáveis, datas de vencimento, categorias dos contratos e detalhes dos fornecedores envolvidos.</font><br><br>" +
+
+                "<big><i><b><font color='black'>5. O aplicativo é gratuito?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; Sim, o aplicativo é gratuito e de código aberto. Isso significa que qualquer pessoa pode acessar, utilizar e contribuir para o desenvolvimento do aplicativo.</font><br><br>" +
+
+                "<big><i><b><font color='black'>6. Quais tecnologias foram utilizadas no desenvolvimento do aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; O aplicativo foi desenvolvido em Kotlin, uma linguagem de programação orientada a objetos, gratuita e de código aberto. Ele é executado na Java Virtual Machine, permitindo compatibilidade com códigos em Java e execução nativa no sistema operacional Android.</font><br><br>" +
+
+                "<big><i><b><font color='black'>7. O aplicativo está disponível para qual sistema operacional?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; O aplicativo está disponível para smartphones Android.</font><br><br>" +
+
+                "<big><i><b><font color='black'>8. Existe algum requisito específico para utilizar o aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; Para utilizar o aplicativo, é necessário possuir um smartphone com sistema operacional Android e acesso à internet.</font><br><br>" +
+
+                "<big><i><b><font color='black'>9. O aplicativo permite fazer pesquisas específicas?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; Sim, o aplicativo possui um recurso de busca que permite aos usuários pesquisarem contratos públicos específicos com base em palavras-chave.</font><br><br>" +
+
+                "<big><i><b><font color='black'>10. Como posso contribuir para o desenvolvimento do aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>&nbsp; Para contribuir com o desenvolvimento do aplicativo, você pode acessar o repositório oficial no GitHub, onde é possível obter o código-fonte, reportar problemas, sugerir melhorias e enviar solicitações de alterações.</font><br><br>" +
+
+                "<big><i><b><font color='black'>11. O aplicativo requer autenticação para acessar os dados?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>  Não é necessário autenticação para navegação nos contatos. No entanto, o recurso de favoritar contratos exige autenticação.</font><br><br>" +
+
+                "<big><i><b><font color='black'>12. Como posso marcar contratos como favoritos no aplicativo?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>  O aplicativo permite que o usuário marque contratos como favoritos para acessá-los rapidamente. Isso pode ser feito na tela de detalhes do contrato, onde é possível encontrar um botão para marcar ou desmarcar o contrato como favorito.</font><br><br>" +
+
+                "<big><i><b><font color='black'>13. O aplicativo está vinculado a alguma plataforma governamental existente?</font></b></i></big><br>" +
+                "<font color='#646464';align='justify'>  O aplicativo mencionado no artigo é desenvolvido de forma independente, não sendo diretamente vinculado a nenhuma plataforma governamental existente. No entanto, ele utiliza dados fornecidos pelo Sistema Integrado de Administração e Serviços Gerais (SIASG) por meio de sua API pública.</font><br><br>"
+
 
         binding.tvHelpData.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY)
 
